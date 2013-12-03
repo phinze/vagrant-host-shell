2a1b4977f66a7a3a33e0b67e1a0e1f1afda10465
Carlos Brito Lage <carlos@compstak.com>
2013-12-03 17:32:08 -0500

    Added support abort_on_nonzero and cwd configs

--- lib/vagrant-host-shell/provisioner.rb
+++ lib/vagrant-host-shell/provisioner.rb
@@ -1,14 +1,20 @@
 module VagrantPlugins::HostShell
   class Provisioner < Vagrant.plugin('2', :provisioner)
     def provision
-      Vagrant::Util::Subprocess.execute(
+      result = Vagrant::Util::Subprocess.execute(
         '/bin/bash',
         '-c',
         config.inline,
-        :notify => [:stdout, :stderr]
+        :notify => [:stdout, :stderr],
+        :workdir => config.cwd
       ) do |io_name, data|
         @machine.env.ui.info "[#{io_name}] #{data}"
       end
+
+      if config.abort_on_nonzero && !result.exit_code.zero?      
+        raise VagrantPlugins::HostShell::Errors::NonZeroStatusError.new(config.inline, result.exit_code)  
+      end
+
     end
   end
 end
