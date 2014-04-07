375c2adc86da914f2d60d1bba1bdf874dabb0fc4
Steven Merrill <steven.merrill@gmail.com>
2014-04-07 17:50:19 -0400

    Start 0.0.5pre. Color output red and green like the shell provisioner.

--- lib/vagrant-host-shell/provisioner.rb
+++ lib/vagrant-host-shell/provisioner.rb
@@ -8,11 +8,19 @@ module VagrantPlugins::HostShell
         :notify => [:stdout, :stderr],
         :workdir => config.cwd
       ) do |io_name, data|
-        @machine.env.ui.info "[#{io_name}] #{data}"
+        options = {
+          new_line: false,
+          prefix: false,
+        }
+
+        color = io_name == :stdout ? :green : :red
+        options[:color] = color # if !config.keep_color
+
+        @machine.env.ui.info(data, options)
       end
 
-      if config.abort_on_nonzero && !result.exit_code.zero?      
-        raise VagrantPlugins::HostShell::Errors::NonZeroStatusError.new(config.inline, result.exit_code)  
+      if config.abort_on_nonzero && !result.exit_code.zero?
+        raise VagrantPlugins::HostShell::Errors::NonZeroStatusError.new(config.inline, result.exit_code)
       end
 
     end
--- lib/vagrant-host-shell/version.rb
+++ lib/vagrant-host-shell/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module HostShell
-    VERSION = '0.0.4'
+    VERSION = '0.0.5pre'
   end
 end
